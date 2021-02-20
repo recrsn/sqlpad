bcb182592c1e1d50a4b05abc1e9510a8edbd5c86
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-02-20 17:15:05 +0530

    Simplify login page

--- client/src/features/session/LoginPage.tsx
+++ client/src/features/session/LoginPage.tsx
@@ -1,14 +1,24 @@
 import React, {useState} from "react";
 import {useHistory, useLocation} from "react-router";
 import {createSession} from "../../lib/api";
+import styles from './LoginPage.module.css';
 import {useSession} from "./sessionHooks";
 
+
+function mask(password: string) {
+    return password.replace(/./g, '*')
+}
 
 export default function LoginPage() {
     const [loggingIn, setLoggingIn] = useState(false);
     const [loginError, setLoginError] = useState("");
 
-    const [url, setUrl] = useState("");
+    const [host, setHost] = useState("localhost");
+    const [port, setPort] = useState("5432");
+    const [user, setUser] = useState("postgres");
+    const [password, setPassword] = useState("");
+    const [db, setDb] = useState("postgres");
+
     const {startSession} = useSession();
     const history = useHistory();
     const location = useLocation();
@@ -18,6 +28,7 @@ export default function LoginPage() {
         try {
             setLoggingIn(true);
             setLoginError("");
+            const url = `postgres://${user}:${password}@${host}:${port}/${db}?sslmode=disable`;
             const response = await createSession(url);
             startSession(response.sessionId)
 
@@ -34,7 +45,7 @@ export default function LoginPage() {
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="py-6 px-10 w-6/12 rounded bg-white shadow-md">
+            <div className="py-6 px-10 w-1/2 rounded bg-white shadow-md">
                 <div>
                     <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
                         Connect to your database
@@ -43,21 +54,41 @@ export default function LoginPage() {
                         <p className="mt-2 p-2 rounded text-md bg-red-300 text-red-800">
                             {loginError}
                         </p>)}
-                    <p className="mt-2 text-sm text-gray-600">
-                        Provide a full URI to your database
-                    </p>
                 </div>
-                <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={connect}>
-                    <div className="rounded-md shadow-sm">
-                        <div>
-                            <label htmlFor="url" className="sr-only">Database URI</label>
-                            <input id="url" name="url" type="url" required
-                                   value={url} onChange={event => setUrl(event.target.value)}
-                                   className="rounded block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"
-                                   placeholder="postgres://postgres:password@127.0.0.1/postgres?sslmode=disable"/>
+                <form className="mt-8 space-y-6" onSubmit={connect}>
+                    <div className={styles.grid}>
+                        <label className="mr-2 mb-2" htmlFor="host">Host</label>
+                        <div className="rounded-md shadow-sm flex flex-row mb-2">
+                            <input id="host" type="text" required
+                                   value={host} onChange={event => setHost(event.target.value)}
+                                   className="mr-1 rounded block w-10/12 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"/>
+                            <label className="sr-only" htmlFor="port">Port</label>
+                            <input id="port" type="number" min="1" max="65535" defaultValue="5432"
+                                   value={port} onChange={event => setPort(event.target.value)}
+                                   className="rounded block w-1/6 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"/>
+                        </div>
+                        <label className="mr-2 mb-2" htmlFor="username">Username</label>
+                        <div className="rounded-md shadow-sm mb-2">
+                            <input id="username" type="text" required
+                                   value={user} onChange={event => setUser(event.target.value)}
+                                   className="rounded block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"/>
+                        </div>
+                        <label className="mr-2 mb-2" htmlFor="password">Password</label>
+                        <div className="rounded-md shadow-sm mb-2">
+                            <input id="password" type="password" required
+                                   value={password} onChange={event => setPassword(event.target.value)}
+                                   className="rounded block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"/>
+                        </div>
+                        <label className="mr-2 mb-2" htmlFor="url">Database</label>
+                        <div className="rounded-md shadow-sm mb-2">
+                            <input type="text" required
+                                   value={db} onChange={event => setDb(event.target.value)}
+                                   className="rounded block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 focus:z-10 sm:text-sm"/>
                         </div>
                     </div>
-
+                    <p className="text-sm text-gray-500 my-2">
+                        Will connect to postgres://{user}:{mask(password)}@{host}:{port}/{db}?sslmode=disable
+                    </p>
                     <div>
                         <button type="submit" disabled={loggingIn}
                                 className="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
